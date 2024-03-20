dd4b0d26288e0a051efb4b01412fdad85b9b253e
skrasek.michael <skrasek.michal@email.cz>
2024-03-20 10:31:22 +0100

    added And unit tests

---
+++ tests/RailwayResult.FunctionalExtensions.Tests/AndTests/TheoryData_R1_AndAsync.cs
@@ -0,0 +1,35 @@
+﻿namespace RailwayResult.FunctionalExtensions.Tests.AndTests;
+
+public sealed class TheoryData_R1_AndAsync : TheoryData<Func<R1, Task<R2>>, R1, R2>
+{
+	public TheoryData_R1_AndAsync()
+	{
+		Add(
+			result => result.AndAsync(_ => Task.FromResult(O.B)),
+			O.A,
+			(O.A, O.B)
+		);
+
+		//and should return input failure result
+		Add(
+			result => result.AndAsync(_ => Task.FromResult(O.B)),
+			Errors.ErrorA,
+			Errors.ErrorA
+		);
+
+		// --- TaskOfR1 ---
+
+		Add(
+			result => Task.FromResult(result).AndAsync(_ => Task.FromResult(O.B)),
+			O.A,
+			(O.A, O.B)
+		);
+
+		//and should return input failure result
+		Add(
+			result => Task.FromResult(result).AndAsync(_ => Task.FromResult(O.B)),
+			Errors.ErrorA,
+			Errors.ErrorA
+		);
+	}
+}
--- tests/RailwayResult.FunctionalExtensions.Tests/GlobalUsings.cs
+++ tests/RailwayResult.FunctionalExtensions.Tests/GlobalUsings.cs
@@ -7,3 +7,4 @@ global using R1 = RailwayResult.Result<RailwayResult.FunctionalExtensions.Tests.
 global using R1N = RailwayResult.Result<RailwayResult.FunctionalExtensions.Tests.Obj?>;
 global using R2 = RailwayResult.Result<(RailwayResult.FunctionalExtensions.Tests.Obj, RailwayResult.FunctionalExtensions.Tests.Obj)>;
 global using R2N = RailwayResult.Result<(RailwayResult.FunctionalExtensions.Tests.Obj?, RailwayResult.FunctionalExtensions.Tests.Obj?)>;
+global using R3 = RailwayResult.Result<(RailwayResult.FunctionalExtensions.Tests.Obj, RailwayResult.FunctionalExtensions.Tests.Obj, RailwayResult.FunctionalExtensions.Tests.Obj)>;
