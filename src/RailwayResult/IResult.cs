1d60f2411b077484f5748678e6f8a5309d9ef43d
skrasek.michael <skrasek.michal@email.cz>
2024-04-16 10:06:20 +0200

    added static methods for abstractions (from error and from value), added unit tests

--- src/RailwayResult/IResult.cs
+++ src/RailwayResult/IResult.cs
@@ -5,9 +5,13 @@ public interface IResult
 	public bool IsSuccess { get; }
 	public bool IsFailure { get; }
 	public Error Error { get; }
+
+	public static abstract IResult FromError<TError>(TError error) where TError : Error;
 }
 
 public interface IResult<TValue> : IResult
 {
 	public TValue Value { get; }
+
+	public static abstract IResult<TValue> FromValue(TValue value);
 }
---
+++ tests/RailwayResult.Tests/ResultCreation_Tests.cs
@@ -0,0 +1,46 @@
+﻿namespace RailwayResult.Tests;
+
+public sealed class ResultCreation_Tests
+{
+	[Fact]
+	public void Result_FromError_Should_ReturnSameResult_AsCtor()
+	{
+		//arrange
+		var expectedResult = new Result(GenericError.ErrorA);
+
+		//act
+		var result = Result.FromError(GenericError.ErrorA);
+
+		//assert
+		result.IsSuccess.Should().Be(expectedResult.IsSuccess);
+		result.Error.Should().BeEquivalentTo(expectedResult.Error);
+	}
+
+	[Fact]
+	public void GenericResult_FromError_Should_ReturnSameResult_AsCtor()
+	{
+		//arrange
+		var expectedResult = new Result<string>(GenericError.ErrorA);
+
+		//act
+		var result = Result<string>.FromError(GenericError.ErrorA);
+
+		//assert
+		result.IsSuccess.Should().Be(expectedResult.IsSuccess);
+		result.Error.Should().BeEquivalentTo(expectedResult.Error);
+	}
+
+	[Fact]
+	public void GenericResult_FromValue_Should_ReturnSameResult_AsCtor()
+	{
+		//arrange
+		var expectedResult = Result<string>.Success("100");
+
+		//act
+		var result = Result<string>.FromValue("100");
+
+		//assert
+		result.IsSuccess.Should().Be(expectedResult.IsSuccess);
+		result.Value.Should().BeEquivalentTo(expectedResult.Value);
+	}
+}
